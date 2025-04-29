c1d99458319515cc99d62f18fe955bb0850d255c
CalebMassre <cmassare830@gmail.com>
2025-04-29 11:23:57 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const tulip = document.querySelector("#tulip");
+function changeContent() {
+    document.getElementById('dynamicContent').innerHTML = '<p>Content has been updated! Welcome to the interactive section!</p>';
+}
 
-tulip.onclick = function() {
-    alert("You selected tulip!");
+document.getElementById('golferForm').onsubmit = function(event) {
+    const golferName = document.getElementById('golferName').value;
+    if (golferName === "") {
+        alert("Please enter a golfer name.");
+        event.preventDefault();
+    }
 };
+
+document.querySelector('button[type="reset"]').addEventListener('click', function() {
+    alert('Form has been reset');
+});
